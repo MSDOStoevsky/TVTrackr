72ba0089bf8a291ac17e9834ab5918271ddb03e1
Dylan Lettinga <dletting@mtu.edu>
2016-09-24 01:57:58 -0400

    Displaying JSON String
    Altered the networking thread to push the result onto the UI thread -
    still a work in progress.

--- TVTrackr/app/src/main/java/com/emptystudio/tvtrackr/AccessWebsite.java
+++ TVTrackr/app/src/main/java/com/emptystudio/tvtrackr/AccessWebsite.java
@@ -1,7 +1,11 @@
 package com.emptystudio.tvtrackr;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -13,14 +17,27 @@ import java.net.URL;
 /**
  * Created by Dylan on 9/21/2016.
  */
-public class AccessWebsite extends AsyncTask<String, Void, JSONObject>{
+public class AccessWebsite extends AsyncTask<String, Void, String>{//third param should be JSONObject
+
+    private Context context; //this allows this thread to update the UI thread
+    private View view; //this allows this thread to update the View Elements
+
+    public AccessWebsite (Context context, View root){
+        this.context = context;
+        this.view = root;
+    }
+
     @Override
-    protected JSONObject doInBackground(String... params) {
+    protected String doInBackground(String... params) {
+        String ret = null;//should be JSONObject
         try {
             URL url = new URL("http://api.tvmaze.com/search/shows?q=" + params[0]);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             try {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line = "";
+                while((line = bufferedReader.readLine()) != null)
+                    ret += line;
                 bufferedReader.close();
             } finally {
                 urlConnection.disconnect();
@@ -28,6 +45,11 @@ public class AccessWebsite extends AsyncTask<String, Void, JSONObject>{
         } catch(Exception e) {
             Log.e("Network", e.toString());
         }
-        return null;
+        return ret;//should be JSONObject
+    }
+
+    protected void onPostExecute(String result) {
+        TextView text = (TextView) view.findViewById(R.id.text);
+        text.setText(result);
     }
 }
--- TVTrackr/app/src/main/java/com/emptystudio/tvtrackr/MainActivity.java
+++ TVTrackr/app/src/main/java/com/emptystudio/tvtrackr/MainActivity.java
@@ -3,11 +3,23 @@ package com.emptystudio.tvtrackr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void displayJSON(View v){
+        new AccessWebsite(getBaseContext(), findViewById(R.id.root)).execute("cory+in+the+house");
     }
 }
