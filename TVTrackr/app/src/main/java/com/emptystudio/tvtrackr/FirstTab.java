bce8ef25f19fa2eb77f5989c9fc0453e71e7b3e2
Dylan Lettinga <dletting@mtu.edu>
2016-10-14 15:24:04 -0400

    Finished Tab UI
    There are still problems with the action bar

---
+++ TVTrackr/app/src/main/java/com/emptystudio/tvtrackr/FirstTab.java
@@ -0,0 +1,31 @@
+package com.emptystudio.tvtrackr;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dylan on 10/14/2016.
+ */
+public class FirstTab extends Fragment{
+
+    public FirstTab() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
